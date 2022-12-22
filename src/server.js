810285ecd257235e91913e524c42dfb77cce2615
kimjunseoo <zz0905k@naver.com>
2022-12-22 10:57:55 +0900

    end #3.11

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 
@@ -8,9 +9,29 @@ const handleListening = () => console.log(`Server listening on port ${PORT}`);
 
 app.listen(PORT, handleListening);
 
+// MiddleWare
+const urlLoggerMiddleWare = (req, res, next) => {
+  console.log(`In URL : ${req.url}`);
+  next();
+}
+
+const timeLoggerMiddleWare = (req, res, next) => {
+  const reqTime = new Date();
+  console.log(`Year : ${reqTime.getFullYear()} /  Month : ${reqTime.getMonth() +1} / Date : ${reqTime.getDate()}`);
+  next();
+}
+
+const secLoggerMiddleWare = (req, res, next) => {
+
+}
+
+const prtLoggerMiddleWare = (req, res, next) => {
+
+}
+
 // Handle Request
 
-const handleHome = (req, res) => {
+const handleHome = (req, res, next) => {
   return res.send("<h1> This page is home </h1>");
 };
 
@@ -26,6 +47,7 @@ const handleLogin = (req, res) => {
   return res.send("<h1> This page is /login </h1>");
 };
 
+app.use(morgan("dev"));
 app.get("/", handleHome);
 app.get("/about", handleAbout);
 app.get("/contact", handleContact);
