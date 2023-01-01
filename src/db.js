b445cac3bead75b9b41178ddf575a1c57495ac42
kimjunseoo <zz0905k@naver.com>
2023-01-01 16:28:55 +0900

    end #6.6 2

---
+++ src/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect("mongodb://127.0.0.1:27017/wetube")
+
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connect to DB!!");
+const handleError = () =>  console.log("DB Error", error);
+
+db.on("error", handleError);
+db.once("open", handleOpen);
